c811ee6dd06db60a81adb2af728e9ebd106b0f26
James Nguyen <james.nguyenn96@gmail.com>
2019-10-14 21:13:14 +1100

    Create js folder to hold the index.js file. First commit made

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// DOM CACHE
+
+let link1 = document.getElementById("first");
+
+let link2 = document.getElementById("second");
+
+
+let link3 = document.getElementById("third");
+
+
+let link4 = document.getElementById("fourth");
+
+
+
